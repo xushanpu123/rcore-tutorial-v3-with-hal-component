1ee81e62c3c796385b703d7f76262586e37efb2c
xushanpu123 <798304030@qq.com>
2024-05-02 17:30:25 +0800

    ch1 on polyhal

--- os/src/console.rs
+++ os/src/console.rs
@@ -1,14 +1,13 @@
-//! SBI console driver, for text output
-
-use crate::sbi::console_putchar;
 use core::fmt::{self, Write};
+
+use polyhal::debug::DebugConsole;
 
 struct Stdout;
 
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        for c in s.chars() {
-            console_putchar(c as usize);
+        for c in s.bytes() {
+            DebugConsole::putchar(c);
         }
         Ok(())
     }
@@ -18,7 +17,7 @@ pub fn print(args: fmt::Arguments) {
     Stdout.write_fmt(args).unwrap();
 }
 
-/// print string macro
+//The implementation of print.
 #[macro_export]
 macro_rules! print {
     ($fmt: literal $(, $($arg: tt)+)?) => {
@@ -26,7 +25,7 @@ macro_rules! print {
     }
 }
 
-/// println string macro
+//The implementation of println.
 #[macro_export]
 macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
