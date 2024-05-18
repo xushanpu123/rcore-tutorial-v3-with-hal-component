b498659ade582b806d040ab55a20f3962ae93027
Youjie Zheng <Azure_stars@126.com>
2024-05-18 15:13:37 +0800

    bug: correct the error variable in test-run.yml

--- tools/external_test.sh
+++ tools/external_test.sh
@@ -4,13 +4,12 @@ WORK_DIR=$1
 # pwd 之后和 $WORK_DIR 拼接为绝对路径
 ABSOLUTE_PATH=$(pwd)
 
-# 如果WORK_DIE不为空，那么就进行拼接
-if [ -n "$WORK_DIR" ]; then
+# 如果WORK_DIE不为空或者不等于 "./"，那么就进行拼接
+if [ "$WORK_DIR" != "" ] && [ "$WORK_DIR" != "./" ]; then
 	ABSOLUTE_PATH+="/$WORK_DIR"
 fi
-
 PATCH_TOOL_DIR=$ABSOLUTE_PATH/tools/patch_tool
-
+echo $PATCH_TOOL_DIR
 # The package which needs to be patched
 # It always be the package which triggers the test
 PATCH_PACKAGE=$2
